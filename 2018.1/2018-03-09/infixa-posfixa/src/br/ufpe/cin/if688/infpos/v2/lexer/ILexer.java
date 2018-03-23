f6e7572af616f47057dbe47fb9cf2ba981a39ef0
Gabriel Melo <gvmgs@cin.ufpe.br>
2018-03-23 14:59:40 -0300

    Ajustes

--- 2018.1/2018-03-09/infixa-posfixa/src/br/ufpe/cin/if688/infpos/v2/lexer/ILexer.java
+++
@@ -1,9 +0,0 @@
-package br.ufpe.cin.if688.infpos.v2.lexer;
-
-import java.io.IOException;
-
-public interface ILexer<T> {
-	T getNextToken() throws IOException;
-	int getLine();
-	int getColumn();
-}
